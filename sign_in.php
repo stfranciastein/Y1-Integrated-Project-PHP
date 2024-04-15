45e9fcadf8b57375f8d513b5f49e1607460bb0b6
Josh Santiago-Francia <N00151146@iadt.ie>
2024-04-15 15:58:33 +0100

    sign in

--- sign_in.php
+++ sign_in.php
@@ -32,17 +32,29 @@ require_once "./etc/locator.php";
 					<h1><a href="index.php">THE HARPER</a></h1>
 				</div>
 				<div class="col_6_login_right width-6">
-					<h2>Log In</h2>
-					<form>
-						<ul>
-							<li><p><strong>E-Mail or Username</strong></p></li>
-							<li><input type="text" name="headline" class="headline_field"></textinput></li>
-							<li><p><strong>Password</strong></p></li>
-							<li><input type="text" name="headline" class="headline_field"></textinput></li>
-						</ul>
-					</form>
-						<p><button class="allbutton yellowbutton">Log In</button> or <button class="allbutton yellowbutton">Sign Up</button></p>
-						<p><a href="story_index.php">Trouble signing in?</a></p>
+					<div class="login_upper">
+						<h1>Log In</h1>
+						<form>
+							<ul>
+								<li><h5>Email or Username</h5></li>
+								<li><input type="text" name="headline" class="headline_field"></textinput></li>
+								<li><h5>Password</h5></li>
+								<li><input type="password" name="headline" class="headline_field"></textinput></li>
+							</ul>
+						</form>
+							<div class="login_or_signup">
+								<p><button class="allbutton yellowbutton">Log In</button></p> 
+								<p>or</p>
+								<p><button class="allbutton yellowbutton">Sign Up</button></p>
+
+							</div>
+							<p><a href="story_index.php">Trouble signing in?</a></p>
+					</div>
+					<div class="login_buttons">
+						<p><i class="fa-brands fa-google"></i> Continue with Google</p>
+						<p><i class="fa-brands fa-apple"></i> Continue with Apple</p>
+						<p><i class="fa-brands fa-facebook"></i> Continue with Facebook</p>
+					</div>
 				</div>
 			</div>
 		</section>
